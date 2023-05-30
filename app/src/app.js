46c930e13be269f5f2e938c5f6347a15a05ab28b
Jonasdegroote1 <61138704+Jonasdegroote1@users.noreply.github.com>
2023-05-30 12:13:43 +0200

    fixing

--- app/src/app.js
+++ app/src/app.js
@@ -20,11 +20,6 @@ import cookieParser from "cookie-parser";
 
 
 //import middleware
-import registerAuthentication from './middleware/validation/registerAuthentication.js';
-import loginAuthentication from './middleware/validation/loginAuthentication.js';
-import {
-  jwtAuth
-} from './middleware/jwtAuth.js';
 import registerAuthentication from "./middleware/validation/registerAuthentication.js";
 import loginAuthentication from "./middleware/validation/loginAuthentication.js";
 import {
@@ -38,9 +33,6 @@ import {
   postUser,
   updateUser,
 } from './controllers/api/user.js';
-updateUser,
-}
-from './controllers/api/user.js';
 import {
   deleteItem,
   getItems,
@@ -64,13 +56,7 @@ app.use(cookieParser());
 
 //serve static files
 app.use(express.static('client/assets'));
-app.use(express.static('client/assets'));
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -81,7 +67,6 @@ app.use(
 const hbs = create({
   // helpers: handlebarsHelpers,
   extname: 'hbs',
-  extname: 'hbs',
   // defaultLayout: "main",
   // layoutsDir: path.resolve("src", "views", "layouts"),
   // partialsDir: path.resolve("src", "views", "partials"),
@@ -90,9 +75,7 @@ const hbs = create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', path.join(SOURCE_PATH, 'views'));
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.set('views', path.join(SOURCE_PATH, 'views'));
+
 
 //-----------------ROUTES --------------//
 app.get("/", jwtAuth, home);
@@ -111,11 +94,6 @@ app.get('/api/users/:id', getSpecificUser);
 app.post('/api/users', postUser);
 app.put('/api/users', updateUser);
 app.delete('/api/users/:id', deleteUser);
-app.get('/api/users', getUsers);
-app.get('/api/users/:id', getSpecificUser);
-app.post('/api/users', postUser);
-app.put('/api/users', updateUser);
-app.delete('/api/users/:id', deleteUser);
 
 //inventory
 app.get('/api/inventory', getItems);
@@ -123,13 +101,8 @@ app.get('/api/inventory/:id', getSpecificItem);
 app.post('/api/inventory', postItem);
 app.put('/api/inventory', updateItem);
 app.delete('/api/inventory/:id', deleteItem);
-app.get('/api/inventory', getItems);
-app.get('/api/inventory/:id', getSpecificItem);
-app.post('/api/inventory', postItem);
-app.put('/api/inventory', updateItem);
-app.delete('/api/inventory/:id', deleteItem);
 
-app.get('/', home);
+
 app.get('/', home);
 const port = process.env.PORT || 3000;
 
@@ -148,5 +121,6 @@ if (process.env.NODE_ENV !== 'test') {
         console.log('Error: ', error);
       });
   }
+}
 
-  export default app;
+export default app
