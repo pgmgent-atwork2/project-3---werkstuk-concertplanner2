79796285672bd7d058388fa862d4d85b59962e17
Jonasdegroote1 <61138704+Jonasdegroote1@users.noreply.github.com>
2023-05-30 11:08:06 +0200

    inventory page with dynamic data

--- app/src/app.js
+++ app/src/app.js
@@ -12,8 +12,9 @@ import {
 // import handlebarsHelpers from "./lib/handlebarsHelpers.js";
 
 import {
-  home
-} from "./controllers/home.js";
+  home,
+  inventory
+} from "./controllers/dashboard.js";
 import bodyParser from "body-parser";
 import DataSource from "./lib/DataSource.js";
 import cookieParser from "cookie-parser";
@@ -22,7 +23,9 @@ import cookieParser from "cookie-parser";
 //import middleware
 import registerAuthentication from "./middleware/validation/registerAuthentication.js";
 import loginAuthentication from "./middleware/validation/loginAuthentication.js";
-import { jwtAuth } from "./middleware/jwtAuth.js";
+import {
+  jwtAuth
+} from "./middleware/jwtAuth.js";
 
 import {
   deleteUser,
@@ -38,7 +41,13 @@ import {
   postItem,
   updateItem
 } from "./controllers/api/inventory.js";
-import { login, logout, postLogin, postRegister, register } from "./controllers/authentication.js";
+import {
+  login,
+  logout,
+  postLogin,
+  postRegister,
+  register
+} from "./controllers/authentication.js";
 
 //create express  app
 const app = express();
@@ -67,12 +76,13 @@ app.set("view engine", "hbs");
 app.set("views", path.join(SOURCE_PATH, "views"));
 
 //-----------------ROUTES --------------//
-app.get("/",jwtAuth, home);
+app.get("/", jwtAuth, home);
 app.get("/login", login)
 app.get("/register", register)
 app.post("/register", registerAuthentication, postRegister, register);
 app.post("/login", loginAuthentication, postLogin, login);
 app.post("/logout", logout);
+app.get("/inventory", jwtAuth, inventory)
 
 
 //API routes
