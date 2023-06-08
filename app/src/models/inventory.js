17ea03c556f1b8b69ca5c77e0de40dc87eef87ad
«pgm-stenhullebroeck» <sten.hullebroeck@student.arteveldehs.be>
2023-06-08 15:34:21 +0200

    haha

---
+++ app/src/models/inventory.js
@@ -0,0 +1,27 @@
+import typeorm from 'typeorm';
+
+const { EntitySchema } = typeorm;
+
+export default new EntitySchema({
+  name: 'inventory',
+  tableName: 'inventory',
+  columns: {
+    id: {
+      primary: true,
+      type: 'int',
+      generated: true,
+    },
+    name: {
+      type: 'varchar',
+    },
+    count: {
+      type: 'int',
+    },
+    length: {
+      type: 'int',
+    },
+    width: {
+      type: 'int',
+    },
+  },
+});
